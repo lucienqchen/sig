92bc614ab543dfe91e642cdd6d10683dcc738565
lucienucsd <lqchen@ucsd.edu>
2023-02-15 16:39:28 -0800

    split code into two functions and added more documnetation

--- rolling_sharpe.py
+++ rolling_sharpe.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 plt.style.use("ggplot")
 
-def rolling_sharpe(start=dt.datetime(2016, 1, 1), end=dt.datetime.now(), ticker=None, window=20):
+def rolling_sharpe(start: dt.datetime, end: dt.datetime, ticker: str, window: int) -> pd.Series:
     
     RISK_FREE_RATE = yf.Ticker("^TYX").history()["Close"][-1] / 100 / 252
     # Gets closing prices of ticker for the past 
@@ -23,9 +23,22 @@ def rolling_sharpe(start=dt.datetime(2016, 1, 1), end=dt.datetime.now(), ticker=
     #Calculate Rolling Sharpe
     sharpe = (log_returns.rolling(window=window).mean() - RISK_FREE_RATE) * window / volatility
     
-    #Plots Rolling Sharpe with labels
-    fig = sharpe.plot()
-    fig.set_title(f"{ticker} Sharpe Ratio")
-    fig.set_ylabel("Sharpe Ratio")    
+    return sharpe
+
+def plot_rs(ticker: str, rs_ser: pd.Series) -> None:
     
-    return fig
+    #Creates parameters for scaling the figure
+    
+    years = pd.to_datetime(rs_ser.index).year
+    width = (years.max() - years.min()) * 2
+    height= abs(rs_ser.max()) + abs(rs_ser.min())
+    
+    #Plots Rolling Sharpe with figsize
+    
+    fig = rs_ser.plot(figsize=(width, height))
+    
+    #Adds title, labels, and horizontal line at 0
+    
+    fig.set_title(f"{ticker} Sharpe Ratio")
+    fig.set_ylabel("Sharpe Ratio")
+    fig.axhline(y=0, color='black', linestyle='-', alpha=0.5)
